0ffe3af05301faa5f7d11b9808e4a458bdb08a45
Joey Lawrance <lawrancej@wit.edu>
2014-07-01 03:36:46 +1000

    Added libc++abi. Not sure if it helps.

--- provision.sh
+++ provision.sh
@@ -3,4 +3,5 @@
 printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 yes | pacman -Syu # YOLO
 yes | pacman -S cmake clang
+yes | pacman -S libc++abi
 yes | pacman -S gcc-multilib
